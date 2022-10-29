6b98428c4899119fc9c718d4b78560c3de8f45ee
DD477 <ddmitriy477@gmail.com>
2022-10-29 18:53:14 +0300

    united login guest test in one class

--- test_main_page.py
+++ test_main_page.py
@@ -5,21 +5,23 @@ from pages.main_page import MainPage
 from pages.cart_page import CartPage
 
 
-@pytest.mark.skip
-def test_guest_can_go_to_login_page(browser):
-    link = 'http://selenium1py.pythonanywhere.com'
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
+@pytest.mark.login_guest
+class TestLoginFromMainPage:
+    def test_guest_can_go_to_login_page(self, browser):
+        link = 'http://selenium1py.pythonanywhere.com'
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
 
-
-def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
-    link = 'http://selenium1py.pythonanywhere.com'
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_cart()
-    cart_page = CartPage(browser, browser.current_url)
-    cart_page.see_that_cart_is_empty()
-    cart_page.see_cart_empty_message()
+    def test_guest_cant_see_product_in_basket_opened_from_main_page(
+        self, browser
+    ):
+        link = 'http://selenium1py.pythonanywhere.com'
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_cart()
+        cart_page = CartPage(browser, browser.current_url)
+        cart_page.see_that_cart_is_empty()
+        cart_page.see_cart_empty_message()
